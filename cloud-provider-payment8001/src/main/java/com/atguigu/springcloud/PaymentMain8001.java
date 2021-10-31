3d473af14402214b1fbff60c91e6cdadd97bb99c
wanggaoliang@jiemian.com <wanggaoliang@jiemian.com>
2021-10-31 22:48:06 +0800

    支付模块开始构建

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -0,0 +1,15 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 王高亮
+ * @create 2021-10-30 22:52
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class,args);
+    }
+}
